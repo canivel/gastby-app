8b78af90abfb96a534af3240e82603ff44177782
dcanivel-at-325274068097 <IESzg7L/tD0VMsuy/YKjuPgawtjynvmwD+rO24MtpGs=>
2019-03-17 22:09:04 -0400

    pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const { createFilePath } = require("gatsby-source-filesystem")
 
+const BlogTemplate = path.resolve("./src/templates/blog-template.js")
 const PostTemplate = path.resolve("./src/templates/post-template.js")
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -38,6 +39,27 @@ exports.createPages = async ({ graphql, actions }) => {
       component: PostTemplate,
       context: {
         slug: post.fields.slug,
+      },
+    })
+  })
+
+  posts.forEach((_, index, postsArr) => {
+    const totalPages = postsArr.lenght
+    const postsPerPage = 1
+    const currentPage = index + 1
+    const isFirstPage = index === 0
+    const isLastPage = currentPage === totalPages
+
+    createPage({
+      path: isFirstPage ? "/blog" : `/blog/${currentPage}`,
+      component: BlogTemplate,
+      context: {
+        limit: postsPerPage,
+        skip: index * postsPerPage,
+        isFirstPage,
+        isLastPage,
+        currentPage,
+        totalPages,
       },
     })
   })
